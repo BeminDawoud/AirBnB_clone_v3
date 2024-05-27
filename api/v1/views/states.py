b08cca19c887da347752996461b5b3c1b77a335c
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-27 11:37:19 +0300

    Update functionality of delete and put

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,35 +43,28 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     ''' adds a new State object: POST '''
-    try:
-        data = request.get_json()
-        if not data:
-            abort(400, "Not a JSON")
-        state_name = data.get("name", None)
-        if not state_name:
-            abort(400, "Missing name")
-        new_state = State(**data)
-        storage.new(new_state)
-        storage.save()
-        return jsonify(new_state.to_dict()), 201
-    except Exception:
-        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    state_name = data.get("name", None)
+    if not state_name:
+        abort(400, "Missing name")
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     ''' adds a new State object: POST '''
-    try:
-        obj = storage.get('State', state_id)
-        if not obj:
-            abort(404)
-        data = request.get_json()
-        if not data:
-            abort(400, "Not a JSON")
-        for key, value in data.items():
-            if (key != "id" and key != "created_at" and key != "updated_at"):
-                setattr(obj, key, value)
-        storage.save()
-        return jsonify(obj.to_dict()), 200
-    except Exception:
+    obj = storage.get('State', state_id)
+    if not obj:
         abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        if (key != "id" and key != "created_at" and key != "updated_at"):
+            setattr(obj, key, value)
+    obj.save()
+    return jsonify(obj.to_dict()), 200
