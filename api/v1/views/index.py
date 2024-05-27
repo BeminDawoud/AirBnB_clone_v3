82c2af1a1b55028002f024e84fb1acf7f90ff38c
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-27 06:13:45 +0300

    Create task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,22 @@
 '''returns a JSON: "status": "OK"'''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats_count():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
