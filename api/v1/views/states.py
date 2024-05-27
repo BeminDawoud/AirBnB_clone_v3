b4a3d41318c51949de8c67dc2ea49600be83f177
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-27 12:05:48 +0300

    Test new post

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -12,16 +12,19 @@ from models.state import State
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id=None):
     ''' Retrieves the list of all State objects: GET'''
-    tmp_obj = []
-    if not state_id:
-        for val in storage.all(State).values():
-            tmp_obj.append(val.to_dict())
-        return jsonify(tmp_obj)
-    item = storage.get(State, state_id)
-    if item:
-        return jsonify(item.to_dict())
+    if state_id:
+        try:
+            obj = storage.get('State', state_id).to_dict()
+            return jsonify(obj)
+        except Exception:
+            abort(404)
+
     else:
-        abort(404)
+        objects_list = []
+        objects = storage.all("State")
+        for value in objects.values():
+            objects_list.append(value.to_dict())
+        return jsonify(objects_list)
 
 
 @app_views.route("/states/<state_id>", methods=['DELETE'],
@@ -43,12 +46,14 @@ def post_state():
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
-    state_name = data.get("name", None)
-    if not state_name:
+    if "name" not in data:
         abort(400, "Missing name")
-    new_state = State(**data)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    try:
+        new_state = State(**data)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
+    except Exception:
+        abort(400, "Not a JSON")
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
