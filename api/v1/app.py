08748055cbb60f9e92fe113af0b2ce63e77bcac7
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-27 05:51:53 +0300

    Update pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
@@ -19,6 +19,6 @@ def teardown():
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    port = getenv("HBNB_API_PORT", "5000")
 
     app.run(host=host, port=port, threaded=True)
