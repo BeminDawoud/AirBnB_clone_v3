4090a55f235c435ae63739f5bbb9e81c2e4bfa72
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-27 06:03:34 +0300

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """ closes the storage """
