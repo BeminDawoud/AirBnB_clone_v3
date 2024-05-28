7e73c02c379f0bcbb7135bac7af8328eb1a36672
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-05-28 04:38:26 +0300

    Update methods

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -27,7 +27,7 @@ def get_place_amenities(place_id=None):
         abort(404)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['GET'],
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place_amenity(place_id=None, amenity_id=None):
     ''' Deletes a Amenity object to a Place: DELETE'''
